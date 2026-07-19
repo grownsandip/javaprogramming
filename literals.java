8e33caab10a6200d1fcf8ef57aaf884e8da934c3
Sandip <roysandip33290@gmail.com>
2026-07-19 19:12:40 +0530

    checking git remote work

--- literals.java
+++ literals.java
@@ -5,7 +5,7 @@ public class literals{
         int  x=9;
         long b=45l;//by default long takes int
         short u=43;
-        float f1=5.6f;
+        float f1=5.6f;//floating point numbers
         double d1=4.66d;
         System.out.print(age + " " + ch + " " + x + " " + b + " " + u + " " + f1 + " " + d1);
     }
